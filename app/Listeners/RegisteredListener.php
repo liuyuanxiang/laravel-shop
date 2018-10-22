e8240f0ffaf0d8b1aa155087d29d5685251e5626
YashonLvan <503931578@qq.com>
2018-10-22 11:52:10 +0800

    触发激活邮件

---
+++ app/Listeners/RegisteredListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RegisteredListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        // 获取到刚刚注册的用户
+        $user = $event->user;
+        // 调用 notify 发送通知
+        $user->notify(new EmailVerificationNotification());
+    }
+}
