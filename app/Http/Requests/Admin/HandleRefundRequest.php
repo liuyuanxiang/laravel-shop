984f9eea491100e81092dbadeb74bcffce3fa257
YashonLvan <503931578@qq.com>
2018-11-09 14:18:06 +0800

    管理后台 - 拒绝退款

---
+++ app/Http/Requests/Admin/HandleRefundRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HandleRefundRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'agree' => ['required','boolean'],
+            // reason 为 agree==false 时必填
+            'reason' =>['required_if:agree,false'],// 拒绝退款时需要输入拒绝理由
+        ];
+    }
+}
