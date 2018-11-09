bca2f1b194b8a91b162b03b9606e5c120f7b1ba7
YashonLvan <503931578@qq.com>
2018-11-09 17:13:25 +0800

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -9,6 +9,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
+use Encore\Admin\Facades\Admin;
 
 class CouponCodesController extends Controller
 {
@@ -54,5 +55,55 @@ class CouponCodesController extends Controller
         });
 
         return $grid;
+    }
+
+    public function create(){
+        return Admin::content(function(Content $content){
+            $content->header('新增优惠券');
+            $content->body($this->form());
+        });
+    }
+
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header('编辑优惠券');
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    protected function form(){
+        return Admin::form(CouponCode::class, function (Form $form) {
+            $form->display('id', 'ID');
+            $form->text('name', '名称')->rules('required');
+            $form->text('code','优惠码')->rules(function($form){
+                if ($id = $form->model()->id) {
+                    return 'nullable|unique:coupon_codes,code,'.$id.',id';
+                } else {
+                    return 'nullable|unique:coupon_codes';
+                }
+            });
+            $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required');
+            $form->text('value', '折扣')->rules(function ($form) {
+                if ($form->type === CouponCode::TYPE_PERCENT) {
+                    // 如果选择了百分比折扣类型，那么折扣范围只能是 1 ~ 99
+                    return 'required|numeric|between:1,99';
+                } else {
+                    // 否则只要大等于 0.01 即可
+                    return 'required|numeric|min:0.01';
+                }
+            });
+            $form->text('total', '总量')->rules('required|numeric|min:0');
+            $form->text('min_amount', '最低金额')->rules('required|numeric|min:0');
+            $form->datetime('not_before', '开始时间');
+            $form->datetime('not_after', '结束时间');
+            $form->radio('enabled', '启用')->options(['1' => '是', '0' => '否']);
+
+            $form->saving(function (Form $form) {
+                if (!$form->code) {
+                    $form->code = CouponCode::findAvailableCode();
+                }
+            });
+        });
     }
 }
