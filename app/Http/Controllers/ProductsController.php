b2693338c0aa055279e8be9ba9f3a3046dcb7480
YashonLvan <503931578@qq.com>
2018-11-27 18:06:35 +0800

    前台商品列表支持按类目筛选

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\OrderItem;
 use Illuminate\Http\Request;
@@ -27,6 +28,21 @@ class ProductsController extends Controller
             });
         }
 
+        // 如果有传入 category_id 字段，并且在数据库中有对应的类目
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 如果这是一个父类目
+            if ($category->is_directory) {
+                // 则筛选出该父类目下所有子类目的商品
+                $builder->whereHas('category', function ($query) use ($category) {
+                    // 这里的逻辑参考本章第一节
+                    $query->where('path', 'like', $category->path.$category->id.'-%');
+                });
+            } else {
+                // 如果这不是一个父类目，则直接筛选此类目下的商品
+                $builder->where('category_id', $category->id);
+            }
+        }
+
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
         // order 参数用来控制商品的排序规则
         if ($order = $request->input('order','')){
@@ -42,11 +58,14 @@ class ProductsController extends Controller
 
         $products = $builder->paginate(16);
 
-        return view('products.index', ['products' => $products,
+        return view('products.index', [
+            'products' => $products,
             'filters'  => [
                 'search' => $search,
                 'order'  => $order,
-            ]]);
+            ],
+            'category' => $category ?? null,
+        ]);
     }
 
     public function show(Product $product, Request $request)
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,6 +7,25 @@
             <!-- 筛选组件开始 -->
             <div class="row">
                 <form action="{{ route('products.index') }}" class="form-inline search-form">
+                    <!-- 面包屑开始 -->
+                    <!-- 添加一个名为 全部 的链接，直接跳转到商品列表页 -->
+                    <a class="all-products" href="{{ route('products.index') }}">全部</a> &gt;
+                    <!-- 如果当前是通过类目筛选的 -->
+                @if ($category)
+                    <!-- 遍历这个类目的所有祖先类目，我们在模型的访问器中已经排好序，因此可以直接使用 -->
+                    @foreach($category->ancestors as $ancestor)
+                        <!-- 添加一个名为该祖先类目名的链接 -->
+                            <span class="category">
+              <a href="{{ route('products.index', ['category_id' => $ancestor->id]) }}">{{ $ancestor->name }}</a>
+            </span>
+                            <span>></span>
+                    @endforeach
+                    <!-- 最后展示出当前类目名称 -->
+                        <span class="category">{{ $category->name }}</span><span> ></span>
+                        <!-- 当前类目的 ID，当用户调整排序方式时，可以保证 category_id 参数不丢失 -->
+                        <input type="hidden" name="category_id" value="{{ $category->id }}">
+                @endif
+                <!-- 面包屑结束 -->
                     <input type="text" class="form-control input-sm" name="search" placeholder="搜索">
                     <button class="btn btn-primary btn-sm">搜索</button>
                     <select name="order" class="form-control input-sm pull-right">
@@ -35,7 +54,9 @@
                                                     </a>
                                                 </div>
                                                 <div class="price"><b>￥</b>{{ $product->price }}</div>
-                                                <div class="title"><a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a></div>
+                                                <div class="title"><a
+                                                            href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+                                                </div>
                                             </div>
                                             <div class="bottom">
                                                 <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span>
@@ -61,7 +82,7 @@
         var filters = {!! json_encode($filters) !!};
         $(document).ready(function () {
             $('.search-form input[name=search]').val(filters.search);
-            $('.search-form select[name=order]').on('change', function() {
+            $('.search-form select[name=order]').on('change', function () {
                 $('.search-form').submit();
             });
         })
